dcf8b1b1de8ed1fec1c3a72c3758f9f4b63d4cfd
Bhupender Kashyap <38746420+bhupenderkashyap122@users.noreply.github.com>
2026-01-28 17:13:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,16 @@ let waitingUser = null;
 io.on("connection", socket => {
   socket.partner = null;
   socket.gender = null;
+  socket.country = null;
 
-  socket.on("join", gender => {
-    socket.gender = gender;
+  socket.on("join", data => {
+    socket.gender = data.gender;
+    socket.country = data.country;
 
     if (
       waitingUser &&
-      (waitingUser.gender === gender || gender === "any" || waitingUser.gender === "any")
+      (waitingUser.gender === socket.gender || socket.gender === "any" || waitingUser.gender === "any") &&
+      (waitingUser.country === socket.country || socket.country === "any" || waitingUser.country === "any")
     ) {
       socket.partner = waitingUser;
       waitingUser.partner = socket;
@@ -31,16 +34,19 @@ io.on("connection", socket => {
     }
   });
 
-  // 🔤 TEXT MESSAGE
+  // 💬 CHAT
   socket.on("message", msg => {
-    if (socket.partner) {
-      socket.partner.emit("message", msg);
-    }
+    if (socket.partner) socket.partner.emit("message", msg);
   });
 
   // ✍️ TYPING
   socket.on("typing", () => {
     if (socket.partner) socket.partner.emit("typing");
+  });
+
+  // 📡 WEBRTC SIGNAL
+  socket.on("signal", data => {
+    if (socket.partner) socket.partner.emit("signal", data);
   });
 
   // ⏭ NEXT
@@ -52,20 +58,17 @@ io.on("connection", socket => {
     }
 
     if (waitingUser === socket) waitingUser = null;
-
     waitingUser = socket;
     socket.emit("waiting");
   });
 
-  // 📡 WEBRTC SIGNAL
-  socket.on("signal", data => {
-    if (socket.partner) socket.partner.emit("signal", data);
-  });
-
+  // ❌ DISCONNECT
   socket.on("disconnect", () => {
     if (waitingUser === socket) waitingUser = null;
     if (socket.partner) socket.partner.emit("partnerDisconnected");
   });
 });
 
-http.listen(3000, () => console.log("http://localhost:3000"));
+http.listen(3000, () =>
+  console.log("Server running http://localhost:3000")
+);
